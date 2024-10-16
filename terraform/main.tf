048fe84d35ea196da54eb9315e87cbad12b251d8
Rajesh Kumar Maddula V S (Tech Mahindra Ltd) <v-rmaddulavs@microsoft.com>
2024-10-16 20:31:34 +0530

    new brnac created

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+data "azurerm_resource_group" "example" {
+  name = "Test_VM"
+}
+
+data "azurerm_virtual_network" "example-vnet" {
+  name                = "v-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+
+}
+
+data "azurerm_subnet" "example" {
+  name                 = "subnet1"
+  virtual_network_name = data.azurerm_virtual_network.example-vnet.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_shared_image" "example-sig" {
+  name                = "windDc2022"
+  gallery_name        = "AzurepackerImages"
+  resource_group_name = "rg-packer-acg"
+  #version             = "1.0.0"
+}
+
+resource "azurerm_network_interface" "example-nic" {
+  name                = "packerpoc-nic"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+data "azurerm_virtual_machine" "example-vm" {
+  name                = "aibtestvm"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+resource "azurerm_windows_virtual_machine" "example-vm" {
+  name                = "packervmtest"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  admin_password      = "Welcome#2024"
+  network_interface_ids = [
+    azurerm_network_interface.example-nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+    source_image_id = data.azurerm_shared_image.example-sig.id  
+}
+
+output "vm_private_ip" {
+  description = "The private IP address of the Windows VM"
+  value       = azurerm_network_interface.example-nic.private_ip_address
+}
+
---
+++ terraform/provider.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features { }
+  use_oidc =true
+  #use_azuread_auth = true
+}
+ 
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.30.0"
+    }
+ }
+
+backend "azurerm" {
+     key = "terraformansible.tfstate"
+     resource_group_name  = "Test_VM"
+     storage_account_name = "tfstatewells"
+     container_name       = "ansible"
+     use_oidc = true    
+     #use_azuread_auth = true
+  }
+}
